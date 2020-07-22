eb847fa8d962fa7b27d06b6568a90a1fd827de7e
David <dc82927n@gmail.com>
2020-07-22 07:53:52 -0400

    add new files

--- Vehicle/src/com/david/Main.java
+++ Vehicle/src/com/david/Main.java
@@ -3,6 +3,14 @@ package com.david;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code here'
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
+
     }
 }
---
+++ Vehicle1/src/com/david/Main.java
@@ -0,0 +1,11 @@
+package com.david;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+    }
+}

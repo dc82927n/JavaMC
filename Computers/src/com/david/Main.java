29659b8c301eba02557537d893c36c10f4dea5dd
David <dc82927n@gmail.com>
2020-07-31 20:27:07 -0400

    computer

--- Computers/src/com/david/Main.java
+++ Computers/src/com/david/Main.java
@@ -4,11 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-
-        Outlander outlander = new Outlander(36);
-        outlander.accelerate(30);
-
-
     }
 }
--- vechicle2/src/com/david/Car.java
+++ vechicle2/src/com/david/Car.java
@@ -4,11 +4,12 @@ public class Car extends Vehicle {
 
     private int wheels;
     private int doors;
+    private int gears;
     boolean inManual;
 
     int currentGear = 0;
 
-    public Car (String name, String size, int wheels, int doors, boolean inManual, int currentGear) {
+    public Car (String name, String size, int wheels, int doors, int gears, boolean inManual) {
         super(name, size);
         this.wheels = wheels;
         this.doors = doors;
@@ -18,7 +19,7 @@ public class Car extends Vehicle {
 
     public void changeGear(int currentGear){
         this.currentGear = currentGear;
-        System.out.println("Car set current() " + this.currentGear + "gear");
+        System.out.println("Car set current() " + this.currentGear + " gear");
     }
 
     public void changeVelocity (int speed, int direction){
@@ -26,8 +27,23 @@ public class Car extends Vehicle {
         System.out.println("changeVelocity: velocity " + speed  +  direction);
     }
 
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
 
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
 
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
 
+    public void setInManual(boolean inManual) {
+        this.inManual = inManual;
+    }
 
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
 }

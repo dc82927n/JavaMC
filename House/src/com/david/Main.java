792803c36855088db8a3e87142207bc4e5e6b3ab
David <dc82927n@gmail.com>
2020-08-02 22:02:38 -0400

    complete house file

--- House/src/com/david/Main.java
+++ House/src/com/david/Main.java
@@ -8,13 +8,19 @@ public class Main {
         Wall wall1 = new Wall("West");
         Wall wall2 = new Wall("East");
         Wall wall3 = new Wall("North");
-        Wall Wall4 = new Wall("South");
+        Wall wall4 = new Wall("South");
 
         Ceiling ceiling = new Ceiling (12, 55);
 
         Bed bed = new Bed ("Modern", 4, 3, 2, 1);
 
         Lamp lamp = new Lamp ("classic", false, 75);
+
+        Bedroom bedroom = new Bedroom ("Tims", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        bedroom.bed();
+        bedroom.getLamp().turnOn();
+
+
 
     }
 }

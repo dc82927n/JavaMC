93089f65c41c97217e59c1b66254115362e2d258
David <dc82927n@gmail.com>
2020-08-04 20:38:43 -0400

    intro to polymorphism

--- encapsulationChallenge/src/com/david/Main.java
+++ encapsulationChallenge/src/com/david/Main.java
@@ -3,6 +3,20 @@ package com.david;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        // let test the printer class.
+
+        // 1. let create an instance of Printer
+        Printer printer = new Printer(99, true);
+        // we created println that prints out the intials page count
+        System.out.println("this is our printer toner Level " + printer.getPagesPrinted());
+        //2. let now print we give the print pages values
+        // two methods of doing this option 1 and then option 2 as you can see below
+        int printPage = printer.printPages(5);
+        // printer.printPages(5);
+        // ** for the constructor parameters we need to getPages Printed and for the function
+        // we can just use the dot notaiton.
+        System.out.println("let print some pages " + printer.printPages(4) + " second print " + printPage);
+
     }
 }
--- encapsulationChallenge/src/com/david/Printer.java
+++ encapsulationChallenge/src/com/david/Printer.java
@@ -17,20 +17,58 @@ public class Printer {
             this.tonerLevel = -1;
         }
 
-
-        this.tonerLevel = tonerLevel;
         this.duplex = duplex;
         this.pagesPrinted = 0;
     }
 
     // we going to add a function that will add to the toner level
-    public void addToner(int tonerAmount){
-        // we need to check how we going to add toner
-        
+    public int addToner (int tonerAmount) {
+        // we need to check how we going to add toner but first less check if
+        // the total amount is less or greater than total amount.
+        if(tonerAmount > 0 && tonerAmount <= 100){
+            // first we want to check if th toner is between 0 and 100
+            // if it is then we want to check if the current toner level
+            // + the toner amount we are adding is greater than 100
+            // if it is then we want to return -1
+            if(this.tonerLevel + tonerAmount > 100){
+                return -1;
+            }
+                // the reason for this is we basically say if all the
+                // condition is meet then we add the toner amount with the tonerLevel
+                // and return the tonerlevel.
+                // same as this.tonerLevel += tonerAmount.
+                this.tonerLevel = tonerLevel + tonerAmount;
+                return this.tonerLevel;
+            } else {
+            // convetion if we return an error.
+            return -1;
+        }
+
+    }
+
+     // creating a function that will determine how many pages from duplex mode
+    public int printPages (int pages){
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("printing in duplex mode");
+        }
+        // not sure why we need this . can we just return pagesTo print?
+        // he says we need to update the pagesPrinted.
+            this.pagesPrinted += pagesToPrint;
+            return pagesToPrint;
+
 
 
     }
 
+
+    // then we going to add a getter for the pagesPrinted to make sure it's correct
+    // why we are not adding getters for everything? 
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
 
 
 }
